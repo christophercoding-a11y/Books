cf6e466222f763686fdb5b751d90eae11ee608d1
christophercodinga11y <christopherjack14@gmail.com>
2025-02-25 14:07:44 -0600

    got the form to work

--- js/app.js
+++ js/app.js
@@ -16,6 +16,7 @@ class Book {
             category: category,
             genre: genre
         }
+        
     }
 
     returnGenres() {
@@ -45,7 +46,9 @@ submitBtn.addEventListener('click', (e)=> {
         obj.category, 
         obj.genres
     )
+
     console.log(book)
+    buildCard(book.bookInfo, book.getDescription)
 })
 
 const getInfo =()=> {
@@ -64,4 +67,64 @@ const getInfo =()=> {
     bookGenres.forEach(item => item.checked ? genres = [...genres, item.value] : null)
 
     return { title, author, year, bookFormat, category, genres}
+
+}
+
+    const buildCard =(obj, func) => {
+        const row = document.getElementById('cardRow')
+
+        const column = document.createElement('div')
+        column.classList.add('col')
+
+        const card = document.createElement('div')
+        card.classList.add('card', 'h-100')
+
+        const cardBody = document.createElement('div')
+        cardBody.classList.add('card-body')
+
+        const title = document.createElement('h3')
+        title.classList.add('card-title', 'text-capitalize', 'text-primary')
+        title.innerText = obj.bookTitle
+
+        const author = document.createElement('p')
+        author.classList.add('card-text', 'text-capitalize', 'text-danger', 'fst-italic')
+        author.innerText = obj.bookAuthor
+
+        const categoryText = document.createElement('p')
+        categoryText.classList.add('card-text', 'text-capitalize')
+        categoryText.innerText = obj.category
+
+        const ul = document.createElement('ul')
+        ul.setAttribute('id', 'genreList')
+
+        obj.genre.forEach(genre => {
+            const li = document.createElement('li')
+            li.classList.add('list-item', 'text-capitalize')
+            li.innerText = genre
+
+            ul.appendChild(li)
+        })
+
+
+    const cardFooter = document.createElement('footer')
+    cardFooter.classList.add('card-footer')
+
+    const description = document.createElement('p')
+    description.classList.add('card-text', 'fst-italic')
+
+    description.innerText = func(obj.bookTitle, obj.bookAuthor, obj.yrReleased)
+
+    cardBody.appendChild(title)
+    cardBody.appendChild(author)
+    cardBody.appendChild(categoryText)
+    cardBody.appendChild(ul)
+
+    cardFooter.appendChild(description)
+
+    card.appendChild(cardBody)
+    card.appendChild(cardFooter)
+
+    column.appendChild(card)
+
+    row.appendChild(column)
 }
